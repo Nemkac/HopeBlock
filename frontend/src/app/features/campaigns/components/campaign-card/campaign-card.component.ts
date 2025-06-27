10a354301f1807b960a449ea54b8c1aa934629d8
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-06-27 23:18:11 +0200

    added donate from card

--- frontend/src/app/features/campaigns/components/campaign-card/campaign-card.component.ts
+++ frontend/src/app/features/campaigns/components/campaign-card/campaign-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Campaign } from '../../../../core/models/campaing';
+import { WalletService } from '../../../../wallet.service';
+import { catchError, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-campaign-card',
@@ -9,9 +11,27 @@ import { Campaign } from '../../../../core/models/campaing';
 export class CampaignCardComponent {
   @Input() campaign!: Campaign;
 
+  constructor(private walletService: WalletService) {
+
+  }
+
   getProgress(): number {
     return this.campaign.goal > 0
       ? Math.min((this.campaign.collected / this.campaign.goal) * 100, 100)
       : 0;
+  }
+
+  donate(): void {
+    this.walletService.connect$().pipe(
+      switchMap(() => this.walletService.donate$(this.campaign.eth_address, '0.01')),
+      catchError(err => {
+        alert('Greška: ' + err.message);
+        return of(null);
+      })
+    ).subscribe(hash => {
+      if (hash) {
+        alert('Transakcija poslata! Hash: ' + hash);
+      }
+    });
   }
 }
